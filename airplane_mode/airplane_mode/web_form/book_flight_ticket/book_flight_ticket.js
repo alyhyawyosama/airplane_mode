c58c6b3201b3c7c0fcac2a8c97769631897dd043
Alyahyawy Osama <alyhyawyosama@gmail.com>
2024-04-05 02:18:12 +0530

    fix : Modified flight_price field to read_only

--- airplane_mode/airplane_mode/web_form/book_flight_ticket/book_flight_ticket.js
+++ airplane_mode/airplane_mode/web_form/book_flight_ticket/book_flight_ticket.js
@@ -1,3 +1,17 @@
+
 frappe.ready(function() {
-	// bind events here
+	frappe.web_form.validate = () => {
+		let data = frappe.web_form.get_values();
+		if (data.flight_price <= 1000000) {
+			frappe.msgprint('Value must be more than 1000');
+			return false;
+		}
+	};
+
+
+	// frappe.web_form.set_df_property([fieldname], [property], [value]);
+	frappe.web_form.set_value("flight_price", "10000")
+	frappe.web_form.set_df_property(["flight_price"], ["read_only"], ["1"]);
+	console.log(frappe.web_form)
+// })
 })
